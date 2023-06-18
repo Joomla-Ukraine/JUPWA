c60ea9058dfbbc7c6223ee818e7aee5e3d0457b8
Denys <denys@joomla-ua.org>
2023-06-19 01:28:27 +0300

    Fix service worker

--- packages/plg_system_jupwa/tmpl/sw.php
+++ packages/plg_system_jupwa/tmpl/sw.php
@@ -17,9 +17,15 @@ defined('_JEXEC') or die('Restricted access');
 $data = (object) $displayData;
 
 ?>
-const CACHE = "jupwa-offline";
+const CACHE = 'jupwa-pages';
 
 importScripts('<?php echo $data->workbox; ?>');
+
+const {registerRoute, setCatchHandler} = workbox.routing;
+const {NetworkFirst, StaleWhileRevalidate, CacheFirst} = workbox.strategies;
+const {CacheableResponsePlugin} = workbox.cacheableResponse;
+const {ExpirationPlugin} = workbox.expiration;
+const {precacheAndRoute, matchPrecache} = workbox.precaching;
 
 const offlineFallbackPage = "/offline.php";
 
@@ -29,6 +35,9 @@ self.addEventListener("message", (event) => {
 	}
 });
 
+// Offline
+self.addEventListener('activate', () => self.clients.claim());
+
 self.addEventListener('install', async (event) => {
 	event.waitUntil(
 		caches.open(CACHE)
@@ -36,14 +45,67 @@ self.addEventListener('install', async (event) => {
 	);
 });
 
+setCatchHandler(async ({event}) => {
+	if (event.request.destination === 'document') {
+		return new matchPrecache(offlineFallbackPage);
+	}
+
+	return new Response.error();
+});
+
+// Preload
 if (workbox.navigationPreload.isSupported()) {
 	workbox.navigationPreload.enable();
 }
 
-workbox.routing.registerRoute(
-	new RegExp('/*'),
-	new workbox.strategies.StaleWhileRevalidate({
-		cacheName: CACHE
+// Cache assets
+registerRoute(
+	({request}) =>
+		request.destination === 'style' ||
+		request.destination === 'script' ||
+		request.destination === 'font',
+	new StaleWhileRevalidate({
+		cacheName: 'jupwa-assets',
+		plugins: [
+			new CacheableResponsePlugin({
+				statuses: [0, 200]
+			})
+		]
+	})
+);
+
+// Cache images
+registerRoute(
+	({request}) => request.destination === 'image',
+	new CacheFirst({
+		cacheName: 'jupwa-images',
+		plugins: [
+			new CacheableResponsePlugin({
+				statuses: [0, 200]
+			}),
+			new ExpirationPlugin({
+				maxEntries: 100,
+				maxAgeSeconds: 30 * 24 * 60 * 60,
+				purgeOnQuotaError: true
+			})
+		]
+	})
+);
+
+// Cache pages
+registerRoute(
+	({request}) => request.mode === 'navigate',
+	new NetworkFirst({
+		cacheName: CACHE,
+		plugins: [
+			new CacheableResponsePlugin({
+				statuses: [0, 200]
+			}),
+			new ExpirationPlugin({
+				maxAgeSeconds: 30 * 24 * 60 * 60,
+				purgeOnQuotaError: true
+			})
+		]
 	})
 );
 
@@ -52,18 +114,14 @@ self.addEventListener('fetch', (event) => {
 		event.respondWith((async () => {
 			try {
 				const preloadResp = await event.preloadResponse;
-
 				if (preloadResp) {
 					return preloadResp;
 				}
-
 				const networkResp = await fetch(event.request);
-
 				return networkResp;
 			} catch (error) {
 				const cache = await caches.open(CACHE);
 				const cachedResp = await cache.match(offlineFallbackPage);
-
 				return cachedResp;
 			}
 		})());
