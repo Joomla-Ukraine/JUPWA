38fccc97c0fe5a48b669bacd9ac6d4cf85ea3da0
Denys <denys@joomla-ua.org>
2025-06-18 01:58:37 +0300

    Add Class Push

---
+++ packages/plg_system_jupwa/libraries/src/Push/Push.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @package     JUPWA\Push
+ * @subpackage
+ *
+ * @copyright   A copyright
+ * @license     A "Slug" license name e.g. GPL2
+ */
+
+namespace JUPWA\Push;
+
+use Curl\Curl;
+
+class Push
+{
+	/**
+	 *
+	 * @param string $serverKey
+	 * @param string $token
+	 * @param string $title
+	 * @param string $body
+	 * @param array  $data
+	 *
+	 * @return array
+	 *
+	 * @throws \JsonException
+	 * @since 1.0
+	 */
+	public static function sendNotification(string $serverKey, string $token, string $title, string $body, array $data = []): array
+	{
+		$url = 'https://fcm.googleapis.com/fcm/send';
+
+		$curl = new Curl();
+		$curl->setHeader('Authorization', 'key=' . $serverKey);
+		$curl->setHeader('Content-Type', 'application/json');
+		$curl->post($url, [
+			'to'           => $token,
+			'notification' => [
+				'title' => $title,
+				'body'  => $body,
+			],
+			'data'         => $data,
+			'priority'     => 'high',
+		]);
+
+		if($curl->error)
+		{
+			throw new \Exception('Curl failed: ' . $curl->getErrorMessage());
+		}
+
+		$result = $curl->response;
+
+		return json_decode($result, true, 512, JSON_THROW_ON_ERROR);
+	}
+}
