c54c69610e93de1d245b4a7d41891feed9614225
Denys <denys@joomla-ua.org>
2025-07-24 10:18:50 +0300

    Check is found subscribe and unsubsribe buttons in DOM

--- src/js/push.js
+++ src/js/push.js
@@ -29,47 +29,78 @@ import jupwaNotification from './modules/notification';
             const app = initializeApp(firebaseConfig),
                 messaging = getMessaging(app);
 
-            let swRegistration = null;
-            if ('serviceWorker' in navigator) {
-                window.addEventListener('load', async () => {
-                    if (/iPhone|iPad|iPod/.test(navigator.userAgent) && !navigator.standalone) {
-                        unsubscribeButton.disabled = true;
-                        subscribeButton.disabled = true;
-
-                        jupwaNotification(lang.addToMainDisplay);
-
-                        return;
-                    }
-
-                    swRegistration = await navigator.serviceWorker.register(urlSW);
-
-                    if (!('PushManager' in window)) {
-                        jupwaNotification(lang.notSupport);
-                        subscribeButton.disabled = true;
-
-                        return;
-                    }
-
-                    if (Notification.permission === 'granted') {
-                        const tokenStorage = localStorage.getItem('jupwaFCMToken');
-                        if (tokenStorage) {
-                            unsubscribeButton.disabled = false;
+            if (subscribeButton && unsubscribeButton) {
+                let swRegistration = null;
+                if ('serviceWorker' in navigator) {
+                    window.addEventListener('load', async () => {
+                        if (/iPhone|iPad|iPod/.test(navigator.userAgent) && !navigator.standalone) {
+                            unsubscribeButton.disabled = true;
                             subscribeButton.disabled = true;
-                        }
-                    } else {
-                        jupwaNotification(lang.notGranted);
-                    }
-                });
-            } else {
-                subscribeButton.disabled = true;
-                unsubscribeButton.disabled = true;
 
-                jupwaNotification(lang.swNotSupport);
+                            jupwaNotification(lang.addToMainDisplay);
+
+                            return;
+                        }
+
+                        swRegistration = await navigator.serviceWorker.register(urlSW);
+
+                        if (!('PushManager' in window)) {
+                            jupwaNotification(lang.notSupport);
+                            subscribeButton.disabled = true;
+
+                            return;
+                        }
+
+                        if (Notification.permission === 'granted') {
+                            const tokenStorage = localStorage.getItem('jupwaFCMToken');
+                            if (tokenStorage) {
+                                unsubscribeButton.disabled = false;
+                                subscribeButton.disabled = true;
+                            }
+                        } else {
+                            jupwaNotification(lang.notGranted);
+                        }
+                    });
+                } else {
+                    subscribeButton.disabled = true;
+                    unsubscribeButton.disabled = true;
+
+                    jupwaNotification(lang.swNotSupport);
+                }
+
+                subscribeButton.addEventListener('click', () => {
+                    subscribe({
+                        csrfToken: csrfToken,
+                        urlSW: urlSW,
+                        urlSubscribe: urlSubscribe,
+                        urlUnSubscribe: urlUnSubscribe,
+                        unsubscribeButton: unsubscribeButton,
+                        subscribeButton: subscribeButton,
+                        messaging: messaging,
+                        firebaseConfig: firebaseConfig,
+                        swRegistration: swRegistration,
+                        lang: lang
+                    });
+                });
+
+                unsubscribeButton.addEventListener('click', () => {
+                    unsubscribe({
+                        csrfToken: csrfToken,
+                        urlSW: urlSW,
+                        urlSubscribe: urlSubscribe,
+                        urlUnSubscribe: urlUnSubscribe,
+                        unsubscribeButton: unsubscribeButton,
+                        subscribeButton: subscribeButton,
+                        messaging: messaging,
+                        firebaseConfig: firebaseConfig,
+                        swRegistration: swRegistration,
+                        lang: lang
+                    });
+                });
             }
 
             onMessage(messaging, (payload) => {
                 const {title, body, image} = payload.notification;
-
                 if (Notification.permission === 'granted') {
                     new Notification(title, {
                         body,
@@ -78,36 +109,6 @@ import jupwaNotification from './modules/notification';
                 }
 
                 jupwaNotification(`${payload.notification.title} — ${payload.notification.body}`);
-            });
-
-            subscribeButton.addEventListener('click', () => {
-                subscribe({
-                    csrfToken: csrfToken,
-                    urlSW: urlSW,
-                    urlSubscribe: urlSubscribe,
-                    urlUnSubscribe: urlUnSubscribe,
-                    unsubscribeButton: unsubscribeButton,
-                    subscribeButton: subscribeButton,
-                    messaging: messaging,
-                    firebaseConfig: firebaseConfig,
-                    swRegistration: swRegistration,
-                    lang: lang
-                });
-            });
-
-            unsubscribeButton.addEventListener('click', () => {
-                unsubscribe({
-                    csrfToken: csrfToken,
-                    urlSW: urlSW,
-                    urlSubscribe: urlSubscribe,
-                    urlUnSubscribe: urlUnSubscribe,
-                    unsubscribeButton: unsubscribeButton,
-                    subscribeButton: subscribeButton,
-                    messaging: messaging,
-                    firebaseConfig: firebaseConfig,
-                    swRegistration: swRegistration,
-                    lang: lang
-                });
             });
         }
     });
