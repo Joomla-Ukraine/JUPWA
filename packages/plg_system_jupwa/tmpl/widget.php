b69257a5a24f6ca96d1392898bcaa513da130b12
Denys <denys@joomla-ua.org>
2025-07-31 12:32:49 +0300

    Add push information

--- packages/plg_system_jupwa/tmpl/widget.php
+++ packages/plg_system_jupwa/tmpl/widget.php
@@ -16,6 +16,9 @@ use Joomla\CMS\Uri\Uri;
 defined('_JEXEC') or die();
 
 $svg = Uri::base() . 'media/jupwa/icons/icons.svg';
+/** @var array $displayData */
+$data  = (object) $displayData;
+$items = (array) $data->params->get('push_widget_info');
 
 ?>
 <div id="jupwa-widget" class="jupwa-widget">
@@ -30,24 +33,41 @@ $svg = Uri::base() . 'media/jupwa/icons/icons.svg';
 				<div class="jupwa-header"><?= Text::_('PLG_JUPWA_WIDGET_HEADER') ?></div>
 				<div class="jupwa-subheader"><?= Text::_('PLG_JUPWA_WIDGET_SUBHEADER') ?></div>
 			</div>
-			<div class="jupwa-animation">
-				<div class="jupwa-pulse">
-					<div class="jupwa-pulse-icon">
-						<svg width="25" height="25">
-							<use xlink:href="<?= $svg; ?>#bell"></use>
-						</svg>
-					</div>
-					<div class="jupwa-pulse-placeholder">
-						<div class="jupwa-pulse-visual"></div>
-						<div class="jupwa-pulse-placeholder-space">
-							<div>
-								<div class="jupwa-pulse-visual jupwa-pulse-visual2"></div>
-								<div class="jupwa-pulse-visual jupwa-pulse-visual1"></div>
+
+			<?php if(is_countable($items) && count($items) > 0): ?>
+				<ul class="jupwa-information">
+					<?php foreach($items as $item): ?>
+						<?php if($item->text): ?>
+							<li>
+								<svg width="20" height="20">
+									<use xlink:href="<?= $svg; ?>#check"></use>
+								</svg>
+								<div><?= $item->text ?></div>
+							</li>
+						<?php endif; ?>
+					<?php endforeach; ?>
+				</ul>
+			<?php else: ?>
+				<div class="jupwa-animation">
+					<div class="jupwa-pulse">
+						<div class="jupwa-pulse-icon">
+							<svg width="25" height="25">
+								<use xlink:href="<?= $svg; ?>#bell"></use>
+							</svg>
+						</div>
+						<div class="jupwa-pulse-placeholder">
+							<div class="jupwa-pulse-visual"></div>
+							<div class="jupwa-pulse-placeholder-space">
+								<div>
+									<div class="jupwa-pulse-visual jupwa-pulse-visual2"></div>
+									<div class="jupwa-pulse-visual jupwa-pulse-visual1"></div>
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
+			<?php endif; ?>
+
 			<div class="jupwa-action">
 				<button id="jupwa-subscribe-btn" class="jupwa-subscribe"><?= Text::_('PLG_JUPWA_WIDGET_SUBSCRIBE_BUTTON') ?></button>
 				<button id="jupwa-unsubscribe-btn" class="jupwa-unsubscribe"><?= Text::_('PLG_JUPWA_WIDGET_UNSUBSCRIBE_BUTTON') ?></button>
