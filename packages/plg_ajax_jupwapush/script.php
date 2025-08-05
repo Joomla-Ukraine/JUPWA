71ee05ee432bd90e94b72aab63bd92723631ef94
Denys <denys@joomla-ua.org>
2025-08-05 20:22:40 +0300

    Changes for DB table

--- packages/plg_ajax_jupwapush/script.php
+++ packages/plg_ajax_jupwapush/script.php
@@ -93,6 +93,78 @@ class plgAjaxJUPWAPushInstallerScript
 	}
 
 	/**
+	 * Called during installation
+	 *
+	 * @param JAdapterInstance $adapter The object responsible for running this script
+	 *
+	 * @return  bool  True on success
+	 * @throws \Exception
+	 * @since     1.0.0
+	 */
+	public function update($adapter): bool
+	{
+		$db      = Factory::getContainer()->get(DatabaseInterface::class);
+		$columns = $db->getTableColumns('#__jupwa_push_orders');
+
+		if(isset($columns[ 'order_desc' ]))
+		{
+			$query = "ALTER TABLE `#__jupwa_push_orders` DROP `order_desc`";
+			$db->setQuery($query);
+			$db->execute();
+		}
+
+		if(isset($columns[ 'user_id' ]))
+		{
+			$columnInfo = $columns[ 'user_id' ];
+
+			if(!(stripos($columnInfo->Type, 'int(11)') !== false && $columnInfo->Null === 'NO'))
+			{
+				$query = "ALTER TABLE `#__jupwa_push_orders` CHANGE `user_id` `user_id` INT(11) NOT NULL DEFAULT 0";
+				$db->setQuery($query);
+				$db->execute();
+			}
+		}
+
+		if(isset($columns[ 'object_group' ]))
+		{
+			$columnInfo = $columns[ 'object_group' ];
+
+			if(!(stripos($columnInfo->Type, 'varchar(155)') !== false && $columnInfo->Null === 'YES' && $columnInfo->Default === null))
+			{
+				$query = "ALTER TABLE `#__jupwa_push_orders` CHANGE `object_group` `object_group` VARCHAR(155) DEFAULT NULL";
+				$db->setQuery($query);
+				$db->execute();
+			}
+		}
+
+		if(isset($columns[ 'order_id' ]))
+		{
+			$columnInfo = $columns[ 'order_id' ];
+
+			if(!(stripos($columnInfo->Type, 'int(11)') !== false && $columnInfo->Null === 'NO'))
+			{
+				$query = "ALTER TABLE `#__jupwa_push_orders` CHANGE `order_id` `order_id` INT(11) NOT NULL DEFAULT 0";
+				$db->setQuery($query);
+				$db->execute();
+			}
+		}
+
+		if(isset($columns[ 'order_url' ]))
+		{
+			$columnInfo = $columns[ 'order_url' ];
+
+			if(!(stripos($columnInfo->Type, 'varchar(155)') !== false && $columnInfo->Null === 'YES' && $columnInfo->Default === null))
+			{
+				$query = "ALTER TABLE `#__jupwa_push_orders` CHANGE `order_url` `order_url` VARCHAR(155) DEFAULT NULL";
+				$db->setQuery($query);
+				$db->execute();
+			}
+		}
+
+		return true;
+	}
+
+	/**
 	 * Called during uninstallation
 	 *
 	 * @param JAdapterInstance $adapter The object responsible for running this script
