2fa54d3664d5a7ea3797375c9508e4edad921377
Denys <denys@joomla-ua.org>
2025-10-06 11:14:48 +0300

    Update dependencies

--- packages/plg_system_jupwa/jupwa.php
+++ packages/plg_system_jupwa/jupwa.php
@@ -157,7 +157,7 @@ class plgSystemJUPWA extends CMSPlugin
 		 */
 		if(Push::isPush([ 'params' => $this->params ]))
 		{
-			$jupwa_js_version = '2.1.0';
+			$jupwa_js_version = '2.1.1';
 			$widget           = Util::tmpl('widget', [
 				'params'  => $this->params,
 				'version' => $jupwa_js_version
@@ -366,7 +366,7 @@ class plgSystemJUPWA extends CMSPlugin
 		}
 
 		$wa               = $doc->getWebAssetManager();
-		$jupwa_js_version = '2.1.0';
+		$jupwa_js_version = '2.1.1';
 
 		if(Push::isPush([ 'params' => $this->params ]))
 		{
